c19573b3afecf2270d9bdbbf0b2d789eee28cb07
shabeensharih <shabeensharih@gmail.com>
2024-10-04 16:15:17 +0530

    tree implemented

---
+++ tree.js
@@ -0,0 +1,21 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null
+    }
+
+    isEmpty(){
+        return this.root === null
+    }
+}
+
+let tree = new BinarySearchTree()
+
+console.log(tree.isEmpty());
